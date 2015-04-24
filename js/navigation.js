17e9e66fc9993ca0bf9ccf7f70478d0b589b09b7
phillip-pilz <info@ppilz.de>
2015-04-24 22:17:16 +0200

    Added Subsites

---
+++ js/navigation.js
@@ -0,0 +1,23 @@
+/**
+ * Created by phillip on 24.04.15.
+ */
+
+function navigateStart(){
+    window.location = "index.html";
+}
+
+function navigateOnlineTournamentPage(){
+    window.location = "OnlineTournament.html";
+}
+
+function navigateLocalTournamentPage(){
+    window.location = "LocalTournament.html";
+}
+
+function navigateLocalStatisticsPage(){
+    window.location = "LocalStatistics.html";
+}
+
+function navigateOnlineStatisticsPage(){
+    window.location = "OnlineStatistics.html";
+}
